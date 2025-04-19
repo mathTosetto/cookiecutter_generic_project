707465dc182329516cf5cb21cb06498fdc3c2e30
Matheus Brizolla <22848645+mathTosetto@users.noreply.github.com>
2025-04-19 19:40:32 +0100

    refactor: init the logger and var envs

--- {{ cookiecutter.project_slug }}/main.py
+++ {{ cookiecutter.project_slug }}/main.py
@@ -1,9 +1,19 @@
+import logging
+
+from dotenv import load_dotenv
+
 from src.utils.my_logger import LoggerSetup
 
-def main():
-    # Initialize the logger setup
-    LoggerSetup()
+# Initialize logger setup
+LoggerSetup()
 
+# Load environment variables
+load_dotenv()
+
+LOGGER: logging.Logger = logging.getLogger("main")
+
+
+def main():
     return True
 
 
